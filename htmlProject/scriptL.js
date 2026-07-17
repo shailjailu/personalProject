ff280b52e52082cb8666811db854dd273ff20022
LNU <2165785@cognizant.com>
2026-07-17 12:18:14 +0530

    rag changes

---
+++ htmlProject/scriptL.js
@@ -0,0 +1,24 @@
+
+// let userName = prompt("Enter your full name");
+// let length = userName.length;
+// let username="";
+
+// for (i in userName){
+//  if(userName[i] !== " "){
+//         username=username + userName[i];
+//     }
+// }
+
+
+// alert(`@${username.toLowerCase()}${length}`);
+
+
+// let str= "Welcome to the game! You have to guess the number correctly. The number is between 1 and 100.";
+
+// console.log(str.slice(2));
+
+function countVowels(str){
+    for (const char of str){
+        console.log(char);
+    }
+}
---
+++ myReact/my-react-app/src/Components/heading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Heading() {
+    let a = 10;
+    let b = 20;
+
+    return (<div>
+        <h1>Hello React!</h1>
+        <p>{a + b}</p>
+    </div>
+    );
+}
+
+export default Heading;
